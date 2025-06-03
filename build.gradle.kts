848abdad09a6a36b86f70a2e087119c9cfbb1138
cyansunday <cyansunday@163.com>
2025-06-03 15:20:18 +0800

    refactor(task-daily): centralizing repositories declaration and adjust build log output model to verbose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    application
+    id("maven-publish")
 }
 
 group = "top.devopsdoc"
@@ -9,6 +10,27 @@ version = "1.0-SNAPSHOT"
 dependencies {
     testImplementation(platform(libs.junitBom))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("top.devopsdoc.Main")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "top.devopsdoc"
+            artifactId = "gradle-demo"
+            version = "1.0-SNAPSHOT"
+            from(components["java"])
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks.test {
